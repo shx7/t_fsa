d4af7f2c525284f81e918f126c7cb333735f1176
alex <lyblinskii.shx@gmail.com>
2015-07-05 21:01:33 -0400

    Refreshed parser skeleton

---
+++ src/headers/DataTypes.h
@@ -0,0 +1,11 @@
+/**************************************************************
+*
+*   PURPOSE: Contains shared data structs, typedefs etc.
+*
+**************************************************************/
+#ifndef DATA_TYPES_H
+#define DATA_TYPES_H
+
+typedef vector<unsigned char> InputSequence;
+
+#endif
